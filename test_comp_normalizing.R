4b4776db2a91d2ad9b583439ea890a02e59a2f9b
Luiz Max Carvalho <luizepidemiologia@gmail.com>
2021-07-29 13:49:54 -0300

    Fixing compilation problems and making things run on older R

--- test_comp_normalizing.R
+++ test_comp_normalizing.R
@@ -1,13 +1,32 @@
-model <- cmdstanr::cmdstan_model("COMP_normalizing.stan")
+library(cmdstanr)
+stanfit <- function(fit) rstan::read_stan_csv(fit$output_files())
+
+model <- cmdstanr::cmdstan_model("COMP_normalizing.stan",
+                                 include_paths = ".")
 
 mu <- 2; nu <- 0.5
 epsilon <- 1e-16; M <- 1e5
 
-opt_adaptive <- adaptive_impl$sample(
+COMP_lpmf <- function(k, theta){
+  mu <- theta[1]
+  nu <- theta[2]
+  return(
+    k * log(mu) - nu*lfactorial(k)  
+  )
+}
+
+comparison <- 
+  
+  model$sample(
   data = list(Mu = mu, Nu = nu, Epsilon = epsilon, maxIter = M,
               TrueValue = matrixStats::logSumExp(
                 sort(
-                  0:M |> {\(k) k * log(mu) - nu * lfactorial(k)}()
+                  COMP_lpmf(k = 0:M, theta = c(mu, nu))
                 )
-              ))
+              )),
+  iter_warmup = 0,
+  iter_sampling = 1,
+  fixed_param = TRUE
 )
+
+comparison
